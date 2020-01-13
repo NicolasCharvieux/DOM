b874b5bc14e8b4a625d52e8d11a891cc4affe48e
NicolasCharvieux <n.charvieux@it-students.com>
2020-01-13 09:12:53 +0000

    Les Dom

---
+++ js.js
@@ -0,0 +1,173 @@
+//todo   EXO1
+
+// var title = document.getElementById("mon_titre");
+// alert(title.innerHTML);
+
+
+//todo   EXO2
+
+// var title = document.getElementById("mon_titre");
+// title.innerHTML = "Titre 1";
+
+
+//todo   EXO3
+
+// var paragraphe = document.getElementById("mon_para");
+// alert(paragraphe.innerHTML);
+
+// var paragraphe = document.getElementById("mon_para");
+// paragraphe.innerHTML = "Blabloblerlehfisdcjhgsf";
+
+
+//todo   EXO4
+
+// function style.color() {
+//     var texte = document.getElementById("mon_titre");
+//     texte.style.color = "red",
+// }
+
+// function green() {
+//     var texte = document.getElementById("mon_titre");
+//     texte.style.color = "green";
+// }
+
+//*en une seule fonction.
+
+// function col() {
+//     var texte = document.getElementById("mon_titre");
+//     if (texte.className == "red") {
+//         texte.className = "green";
+//     } else if (texte.className == "green") {
+//         texte.className = "red";
+//     }
+// }
+
+
+//todo   EXO5
+
+// var j = document.getElementById("one");
+// var k = document.getElementById("two");
+// var l = document.getElementById("three");
+
+// function un() {
+//     j.className = "visible";
+//     k.className = "invisible";
+//     l.className = "invisible";
+// }
+
+// function deux() {
+//     j.className = "invisible";
+//     k.className = "visible";
+//     l.className = "invisible";
+// }
+
+// function trois() {
+//     j.className = "invisible";
+//     k.className = "invisible";
+//     l.className = "visible";
+// }
+
+
+//todo   EXO6
+
+// var j = document.getElementById("image2");
+// var k = document.getElementById("image3");
+// var l = document.getElementById("image1");
+// var a = document.getElementById("bouton2");
+// var b = document.getElementById("bouton3");
+// var c = document.getElementById("bouton1");
+
+// function img2() {
+//     j.className = "invisible";
+//     a.className = "invisible";
+//     k.className = "visible";
+//     b.className = "visible";
+//     l.className = "invisible";
+//     c.className = "invisible"
+// }
+
+// function img3() {
+//     j.className = "invisible";
+//     k.className = "invisible";
+//     l.className = "visible";
+//     a.className = "invisible";
+//     b.className = "invisible";
+//     c.className = "visible";
+// }
+
+// function img1() {
+//     j.className = "visible";
+//     k.className = "invisible";
+//     l.className = "invisible";
+//     a.className = "visible";
+//     b.className = "invisible";
+//     c.className = "invisible";
+// }
+
+
+//todo   EXO7
+
+// var texte = document.body;
+// var titre = document.createElement("h1");
+// var para = document.createElement("p");
+// titre.innerHTML = "YOP !";
+// para.innerHTML = "Salutationnessss ! :P";
+
+// texte.prepend(titre);
+// texte.append(para);
+
+
+//todo   EXO8
+
+// var p = document.getElementById("ph1");
+// var q = document.getElementById("ph2");
+// var r = document.getElementById("ph3");
+// var s = document.getElementById("ph4");
+
+// function dom1() {
+//     p.remove("ph1");
+// }
+
+// function dom2() {
+//     q.remove("ph2");
+// }
+
+// function dom3() {
+//     r.remove("ph3");
+// }
+
+// function dom4() {
+//     s.remove("ph4");
+// }
+
+
+//todo   EXO9
+
+// var m = document.getElementById("ph1");
+// var para = document.createElement("p");
+
+// function butav() {
+//     var text = document.body;
+//     para.innerHTML = "Agrego texto ANTES !";
+//     text.insertBefore(para, m);
+// }
+
+// function butap() {
+//     var text = document.body;
+//     para.innerHTML = "Agrego texto DESPUÉS !";
+//     text.append(para);
+// }
+
+
+//todo   EXO10
+
+// var z = document.getElementById("chang");
+// var titre = document.createElement("h1");
+// titre.innerHTML = "TITRE REMPLACÉ EN h1";
+
+// function cli() {
+//     document.body.replaceChild(titre, z);
+// }
+
+//todo    EXO11
+
